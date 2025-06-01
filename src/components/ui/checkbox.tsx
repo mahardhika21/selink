c367f1e8b7e939434284021d50c8639b2cd8fbeb
Irfan <uiuxirfan@gmail.com>
2025-06-01 06:44:10 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button type="button" role="checkbox" aria-checked=true data-state="checked" value="on" className="peer shrink-0 rounded-sm border-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 h-4 w-4 border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground" aria-label="Select link Wajah Lain Raja Ampat dan Potret Tanah Papua Terkini! - YouTube">
    
    Relevant files:
    
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_af97115a._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/src_fab6caa7._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Perbaiki bentuk checkbox ketika checked agar icon checknya rata tengah dan sedikit lebih tebal

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -21,7 +22,7 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <Check className="h-4 w-4" />
+      <Check className="h-4 w-4" strokeWidth={2.5} />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
