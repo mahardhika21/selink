41097bac8ec6a7160565ae40aa5f1c5c4dfc00ac
Irfan <uiuxirfan@gmail.com>
2025-06-02 10:17:34 +0000

    Buat softshadow di sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -183,6 +183,7 @@ const Sidebar = React.forwardRef<
         <div
           className={cn(
             "flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground",
+            (variant === 'sidebar' || variant === 'inset') && 'shadow-lg',
             className
           )}
           ref={ref}
@@ -199,7 +200,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden shadow-lg"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -249,7 +250,11 @@ const Sidebar = React.forwardRef<
         >
           <div
             data-sidebar="sidebar"
-            className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
+            className={cn(
+              "flex h-full w-full flex-col bg-sidebar",
+              (variant === 'sidebar' || variant === 'inset') && 'shadow-lg',
+              variant === 'floating' && 'rounded-lg border border-sidebar-border shadow'
+            )}
           >
             {children}
           </div>
