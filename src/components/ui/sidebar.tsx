dbbfb3625edd96d74f808975e0c19e6a8a34affd
Irfan <uiuxirfan@gmail.com>
2025-06-02 10:13:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Sidebar side="left" variant="sidebar" collapsible="offcanvas">
    
    with the text content:
    
        CategoriesAdd Category All Links UncategorizedNo categories yet.
    
    Relevant files:
    
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/src_e62a8f06._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748315168093.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Remove stroke line

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -241,7 +242,7 @@ const Sidebar = React.forwardRef<
             // Adjust the padding for floating and inset variants.
             variant === "floating" || variant === "inset"
               ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+2px)]"
-              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l",
+              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon]",
             className
           )}
           {...props}
